c45f672e7d01c2c9ff2845ebe9e7d6c051164f3d
Maksim Pogrebnoi <maxlanser@gmail.com>
2024-02-22 00:25:36 +0200

    initial

---
+++ gulpfile.js
@@ -0,0 +1,187 @@
+const { src, dest, series, parallel, watch } = require('gulp');
+const pug = require('gulp-pug');
+const sass = require('gulp-sass')(require('sass'));
+const rename = require("gulp-rename");
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const clean = require('gulp-clean');
+const browserSync = require('browser-sync').create();
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
+const newer = require('gulp-newer');
+const woff = require('gulp-ttf2woff');
+const woff2 = require('gulp-ttf2woff2');
+const babel = require("gulp-babel");
+const concat = require('gulp-concat');
+const gulpBabelMinify = require('gulp-babel-minify');
+
+const svgSprite = require('gulp-svg-sprite');
+
+
+
+
+function bSync(done) {
+  browserSync.init({
+    server: {
+        baseDir: "./dev"
+    },
+    open: false
+  });
+  done();
+}
+
+function images() {
+  return src('./src/images/**/*.+(jpg|JPG|png|PNG)')
+    .pipe(newer('./dev/images'))
+    .pipe(imagemin([
+      imagemin.mozjpeg({quality: 75, progressive: true}),
+      imagemin.optipng({optimizationLevel: 5})
+    ]))
+    .pipe(dest('./dev/images/'));
+}
+
+function sprite() {
+  return src('./src/images/icons/sprite.svg')
+    .pipe(dest('./dev/images/icons/'));
+}
+
+function copySvg() {
+  return src(['./src/images/icons/*.svg', '!./src/images/icons/sprite.svg'])
+    .pipe(dest('./dev/images/icons'));
+}
+
+function images2webp(){
+  return src('./src/images/**/*.+(jpg|JPG|png|PNG)')
+    .pipe(newer('./dev/images'))
+    .pipe(webp())
+    .pipe(dest('./dev/images'));
+}
+
+function fonts(){
+  return src('./src/fonts/*.*')
+    .pipe(dest('./dev/fonts'));
+}
+
+function fonts2woff(){
+  return src('./src/fonts/*.ttf')
+    .pipe(woff())
+    .pipe(dest('./dev/fonts'));
+}
+
+function fonts2woff2(){
+  return src('./src/fonts/*.ttf')
+    .pipe(woff2())
+    .pipe(dest('./dev/fonts'));
+}
+
+function images2avif(){
+  return src('./src/images/**/*.+(jpg|JPG|png|PNG)')
+    .pipe(newer('./dev/images'))
+    .pipe(avif({
+      quality: 50
+    }))
+    .pipe(dest('./dev/images'));
+}
+
+function script(){
+  return src([
+    "./node_modules/swiper/swiper-bundle.min.js",
+    "./src/js/libs/*.js",
+    "./src/js/main.js"
+    ])
+    //.pipe(babel({presets: ["@babel/preset-env"]}))
+    .pipe(concat('app.js'))
+    //.pipe(gulpBabelMinify())
+    .pipe(dest("./dev/js"));
+}
+
+function cleanDev(){
+  return src('./dev/', {read: false, allowEmpty: true})
+      .pipe(clean());
+}
+
+function pugTask(){
+  return src('./src/pug/pages/*.pug')
+    .pipe(
+      pug({
+        basedir: './src/pug/',
+        pretty: true
+      })
+    )
+    .pipe(dest('./dev'));
+}
+
+function generateSprite() {
+  const svgspriteConfig = {
+    shape: {
+      spacing: { // Add padding
+        padding: 1
+      },
+      dest: 'images/intermediate-svg' // Keep the intermediate files
+    },
+    mode: {
+      css: { // Activate the «css» mode
+        render: {
+          scss: true // Activate CSS output (with default options)
+        },
+        dest: 'scss/',
+        dimensions: true,
+        sprite: '../images/icons/sprite.svg',
+        prefix: '.svg-icon--%s',
+        bust: false
+      }
+    }
+  };
+  return src(['./src/images/icons/*.svg', '!./src/images/icons/sprite.svg'])
+    .pipe(imagemin([
+      imagemin.svgo({
+        plugins: [
+          {removeViewBox: true},
+          {cleanupIDs: false}
+        ]
+      })
+    ]))
+    .pipe(svgSprite(svgspriteConfig))
+    .pipe(dest('./src/'));
+}
+
+function styles() {
+  return src('./src/scss/**/main.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+			cascade: false
+		}))
+    .pipe(rename('style.css'))
+    .pipe(sourcemaps.write('maps/'))
+    .pipe(dest('./dev/css'))
+    .pipe(browserSync.stream());
+};
+
+
+function watching() {
+  watch('./src/pug/**/*.pug').on("change", pugTask);
+  watch('./src/scss/**/*.scss', styles);
+  watch('./src/js/**/*.js', script);
+  watch(['./dev/**/*.*', '!./dev/css/**/*.*']).on("change", browserSync.reload);
+}
+
+exports.html = pugTask;
+exports.styles = styles;
+exports.cleandev = cleanDev;
+exports.svg = generateSprite;
+
+exports.dev = series(parallel(
+  pugTask,
+  styles,
+  script,
+  images,
+  copySvg,
+  series(generateSprite, sprite),
+  fonts,
+  //images2webp,
+  //images2avif,
+  //fonts2woff,
+  //fonts2woff2
+  ), bSync, watching);
---
+++ src/js/main.js
@@ -0,0 +1,51 @@
+(() => {
+  document.addEventListener('DOMContentLoaded', () => {
+    const newsSwiper = new Swiper(".js-news-slider", {
+      navigation: {
+        nextEl: ".js-news-slider__next",
+        prevEl: ".js-news-slider__prev",
+      },
+      pagination: {
+        el: ".js-news-slider__pagination",
+        clickable: true
+      },
+    });
+
+    const vacanciesSwiper = new Swiper(".js-vacancies-slider", {
+      slidesPerView: 4,
+      spaceBetween: 30,
+      navigation: {
+        nextEl: ".js-vacancies__slider-next",
+        prevEl: ".js-vacancies__slider-prev",
+      }
+    });
+
+    const categoriesSwiper = new Swiper(".js-categories-news-slider", {
+      slidesPerView: 4,
+      spaceBetween: 30,
+      navigation: {
+        nextEl: ".js-categories-news__slider-next",
+        prevEl: ".js-categories-news__slider-prev",
+      }
+    });
+
+    const coursesSwiper = new Swiper(".js-courses-slider", {
+      slidesPerView: "auto",
+      spaceBetween: 30,
+      navigation: {
+        nextEl: ".js-courses-slider-next",
+        prevEl: ".js-courses-slider-prev",
+      }
+    });
+
+    const lastWeekNewsSwiper = new Swiper(".js-last-week-news-slider", {
+      slidesPerView: "auto",
+      spaceBetween: 30,
+      navigation: {
+        nextEl: ".js-last-week-news-slider-next",
+        prevEl: ".js-last-week-news-slider-prev",
+      }
+    });
+  });
+
+})();
